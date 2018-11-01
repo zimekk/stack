83701d5dd9cfe280ef5ea8cd4bb076fbe76f2f9f
Marcin Makarewicz <marcin@makarewicz.eu>
2018-11-01 22:44:08 +0100

    Updates

---
+++ precache-manifest.65afe57051f9b5420f1f8c97b3f8d855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af748e32c3029272c915",
+    "url": "/static/js/main.af748e32.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "af748e32c3029272c915",
+    "url": "/static/css/main.96d203b2.chunk.css"
+  },
+  {
+    "revision": "f8f17466e23a8fd09964f83a015e353c",
+    "url": "/index.html"
+  }
+];

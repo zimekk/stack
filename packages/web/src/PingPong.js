ed2bc683f2000342f3e042f021b800f68149f4da
Marcin Makarewicz <marcin@makarewicz.eu>
2018-11-03 01:33:35 +0100

    use graphql-tag

--- packages/web/src/PingPong.js
+++ packages/web/src/PingPong.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { print } from "graphql";
 import { request } from 'graphql-request'
+import gql from 'graphql-tag.macro';
 
 const Button = ({ children, ...props }) => (
   <button {...props}>{children}</button>
@@ -9,32 +11,34 @@ const Message = ({ children }) => (
   <div>{children}</div>
 );
 
+const query = (query, value) => request('/api', print(query), value);
+
 export default class PingPong extends Component {
   state = {
     counter: 1,
     messages: []
   };
   componentDidMount() {
-    request('/api', `{
+    query(gql`query {
       messages {
         text
       }
     }`).then(data =>
-      console.log(data) || data.messages.map(({text}) => text)).then(messages => 
+      console.log(data) || data.messages.map(({ text }) => text)).then(messages => 
       setTimeout(() => this.setState({ messages }), 800)
     )
   }
-  onSend = () => request('/api', `mutation($text: String!) {
+  onSend = () => query(gql`mutation($text: String!) {
       Message {
-        add(input: {text: $text}) {
+        add(input: { text: $text }) {
           text
         }
       }
   }`, { text: `Message #${this.state.counter}` }).then(data =>
-    console.log(data) || data.Message.add.map(({text}) => text)).then(messages => 
+    console.log(data) || data.Message.add.map(({ text }) => text)).then(messages => 
     setTimeout(() => this.setState({ counter: this.state.counter + 1, messages }), 800)
   );
-  onClear = () => request('/api', `mutation {
+  onClear = () => query(gql`mutation {
     Message {
       clear
     }

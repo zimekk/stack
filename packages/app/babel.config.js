e690754d52665ac997f9cf7c4412b24a09d0eabb
Marcin <marcio.moch@gmail.com>
2018-11-17 15:09:15 +0100

    fixed starting packages/app

--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -2,6 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["transform-inline-environment-variables", "macros"]
+    plugins: [
+      "@babel/plugin-transform-runtime",
+      "transform-inline-environment-variables",
+      "macros"
+    ]
   };
 };
